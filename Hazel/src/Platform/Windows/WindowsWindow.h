df844f2dcfab7dcd49ee701c1eba4b03fa8545d5
Lin Chang <lilyforeverx@gmail.com>
2023-11-26 21:18:29 +0800

    Add input polling

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -24,6 +24,8 @@ namespace Hazel
 		void SetVSync(bool enable) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_Window; };
+
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
