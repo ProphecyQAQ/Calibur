86c523e02ea2fe68d4b0977ad110f4fa0f3c8782
Lin Chang <lilyforeverx@gmail.com>
2023-11-30 20:30:51 +0800

    Addd shader class

--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,7 +22,7 @@ namespace Hazel
 		HZ_CORE_INFO("OpenGL Info:");
 		HZ_CORE_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
 		HZ_CORE_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
-		HZ_CORE_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));
+		HZ_CORE_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));  
     }
 
 	void OpenGLContext::SwapBuffers()
