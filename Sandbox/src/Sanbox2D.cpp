60581ec7f5be1b776ecfdac5b2e47f9c2605e225
Lin Chang <lilyforeverx@gmail.com>
2023-12-11 22:22:49 +0800

    Start 2d render

--- Sandbox/src/Sanbox2D.cpp
+++ Sandbox/src/Sanbox2D.cpp
@@ -14,30 +14,7 @@ Sanbox2D::Sanbox2D()
 
 void Sanbox2D::OnAttach()
 {
-	m_SquareVA = Hazel::VertexArray::Create();
 
-	float squareVertices[4 * 5] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-	};
-
-	Hazel::Ref<Hazel::VertexBuffer> suqareVB(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-	
-	Hazel::BufferLayout layout = {
-		{Hazel::ShaderDataType::Float3, "a_Position"},
-	};
-	suqareVB->SetLayout(layout);
-	m_SquareVA->AddVertexBuffer(suqareVB);
-
-	unsigned int squareIndics[6] = { 0, 1, 2, 2, 3, 0 };
-	Hazel::Ref<Hazel::IndexBuffer> squareIB(Hazel::IndexBuffer::Create(squareIndics, std::size(squareIndics)));
-
-	m_SquareVA->SetIndexBuffer(squareIB);
-
-	//Init shader
-	m_FlatColorShader = Hazel::Shader::Create("assets/shaders/FlatColor.glsl");
 }
 
 void Sanbox2D::OnDetach()
@@ -53,18 +30,20 @@ void Sanbox2D::OnUpdate(Hazel::TimeStep ts)
 	Hazel::RenderCommand::SetClearColor(glm::vec4(0.1f, 0.1f, 0.1f, 1.0f));
 	Hazel::RenderCommand::Clear();
 
-	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
+	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.5f));
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+	Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, m_SquareColor);
+
+	//glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.5f));
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
+	//std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 	
-	Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, scale);
+	//Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, scale);
 
 	//Triangle
 	//Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 
-	Hazel::Renderer::EndScene();
+	Hazel::Renderer2D::EndScene();
 
 }
 
