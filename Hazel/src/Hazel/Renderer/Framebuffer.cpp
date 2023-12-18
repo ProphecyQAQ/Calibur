c4f2ab0c0ab98f45d20c926d8eed2099ca27477c
Lin Chang <lilyforeverx@gmail.com>
2023-12-18 21:38:53 +0800

    Add framebuffer class

---
+++ Hazel/src/Hazel/Renderer/Framebuffer.cpp
@@ -0,0 +1,24 @@
+#include "hzpch.h"
+#include "Hazel/Renderer/Framebuffer.h"
+#include "Hazel/Renderer/Renderer.h"
+
+#include "Platform/OpenGL/OpenGLFrameBuffer.h"
+
+namespace Hazel
+{
+	Ref<Framebuffer> Framebuffer::Create(const FramebufferSpecification& spec)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			HZ_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+			return nullptr;
+
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLFramebuffer>(spec);
+		}
+		
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
